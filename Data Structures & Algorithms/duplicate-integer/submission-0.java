51177ebd30f383e59ec34c7d85947bd8f5cad231
Sneha Kumari <kumarisneha3824@gmail.com>
2026-08-16 19:51:15 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> result= new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(result.contains(nums[i])){
+                return true;
+            }else{
+                result.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
