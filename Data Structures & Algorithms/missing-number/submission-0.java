7357d1b8ed7d718ccb28e86570136703a93f9a0f
Sneha Kumari <kumarisneha3824@gmail.com>
2026-08-18 13:08:42 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int result= n;
+        for(int i=0; i<n; i++){
+            result=result^i^nums[i];
+        }
+
+        return result;
+    }
+}
